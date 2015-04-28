4db9f45f9819d29e9e91e57487d651c8962d8141
ferrakkem <ferrakkem@gmail.com>
2015-04-28 18:44:07 +0600

    parsing json data in objective c

--- Parse Json data/main.m
+++ Parse Json data/main.m
@@ -8,10 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSDictionary *jSonData = @{@"username" :@ "8801710540786",@"profile-status":@" ",@"email":@"mansur@divine-it.net",
+                                   @"imei" :@"355810055390325",@"profile-name":@" ",@"fb-user":@" ",@"profile-pic":@" ",
+                                   @"type":@"signup",@"app-version":@"1.8.9",@"opcode":@"1000"};
+      
+        
+        NSData *jsonDicData =[NSJSONSerialization dataWithJSONObject:jSonData options:NSJSONWritingPrettyPrinted error:Nil];
+    
+
+        NSLog(@" %@",[[NSString alloc] initWithData:jsonDicData encoding:NSUTF8StringEncoding]);
+       
+        
+        NSDictionary *decodeDict =[NSJSONSerialization JSONObjectWithData:jsonDicData options:0 error:Nil];
+        
+        
+        assert([jSonData isEqual:decodeDict]);
+        
     }
     return 0;
 }
